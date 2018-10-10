676d0b3790a9f1b71842994aab02d1d9653b84c6
Tom McFarlin <tmcfarlin412@gmail.com>
2018-10-10 19:06:38 -0400

    updates

---
+++ assets/images/Tom McFarlin Consulting_files/header.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    document.querySelector('.mdc-top-app-bar__navigation-icon').addEventListener('click', function () {
+        window.open('/', '_self')
+    })
+});
